6f4ed7d163dae1467051ca0790e99eb8eea7bbce
carlosEA28 <carloseaduarte28@gmail.com>
2025-08-17 11:03:51 -0300

    feat: add sidebar button to sidebar

--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -1,16 +1,29 @@
+import { LayoutGrid, Package, ShoppingBasketIcon } from "lucide-react";
+import SidebarButton from "./sidebar-button";
+
 const Sidebar = () => {
   return (
     <div className="h-screen w-64 bg-white">
-      {/* IMAGE */}
       <div className="px-8 py-6">
         <h1 className="text-2xl font-bold">STOCKLY</h1>
       </div>
 
       {/* BOTOES */}
       <div className="flex flex-col gap-2 p-2">
-        <button className="px-6 py-3">Dashborad</button>
-        <button className="px-6 py-3">Produtos</button>
-        <button className="px-6 py-3">Vendas</button>
+        <SidebarButton href="/">
+          <LayoutGrid size={20} />
+          Dashboard
+        </SidebarButton>
+
+        <SidebarButton href="/products">
+          <Package size={20} />
+          Produtos
+        </SidebarButton>
+
+        <SidebarButton href="/sales">
+          <ShoppingBasketIcon size={20} />
+          Vendas
+        </SidebarButton>
       </div>
     </div>
   );
