6f9068a5b15ca71acf15ce77924939a598223f04
carlosEA28 <carloseaduarte28@gmail.com>
2025-08-17 11:03:31 -0300

    feat: add Sidebar button component

---
+++ app/_components/sidebar-button.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import React from "react";
+
+interface SidebarButtonProps {
+  href: string;
+  children: React.ReactNode;
+}
+
+const SidebarButton = ({ href, children }: SidebarButtonProps) => {
+  const pathName = usePathname();
+
+  return (
+    <Button
+      variant={pathName === `${href}` ? "secondary" : "ghost"}
+      className="cursor-pointer justify-start gap-2"
+      asChild
+    >
+      <Link href={href}>{children}</Link>
+    </Button>
+  );
+};
+
+export default SidebarButton;
